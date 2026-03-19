83d96ae65cd71e458831d3ee682ac782e263a454
Gianna Ross <GRoss10@my.gcu.edu>
2026-03-19 11:06:55 -0700

    Commit frontend types relocation

--- Backend/MakesCentsBackend/obj/Debug/net8.0/MakesCentsBackend.AssemblyInfo.cs
+++ Backend/MakesCentsBackend/obj/Debug/net8.0/MakesCentsBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MakesCentsBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8988f29db822ab6740d6737de693682cfd21a353")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb4aa1db0a119035daa51e0525f2ff87bd5e751")]
 [assembly: System.Reflection.AssemblyProductAttribute("MakesCentsBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MakesCentsBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
