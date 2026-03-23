de701e797e5a13f5aa832bbe39eef943a1c96ba2
Gianna7055 <GRoss10@my.gcu.edu>
2026-03-23 05:50:19 -0700

    Update for Retirement401K403B enum

--- Frontend/MakesCentsFrontend/components/accounts/account-card.tsx
+++ Frontend/MakesCentsFrontend/components/accounts/account-card.tsx
@@ -77,6 +77,8 @@ export const getAccountSubType = (account: SummaryAccountDTOModel): string => {
     return formatEnum(account.debtAccountType.toString());
   if (account.investmentAccountType) {
     if (account.investmentAccountType.toString() == "IRA") return "IRA";
+    if (account.investmentAccountType.toString() == "Retirement401K403B")
+      return "401K / 403B";
     return formatEnum(account.investmentAccountType.toString());
   }
   return "";
