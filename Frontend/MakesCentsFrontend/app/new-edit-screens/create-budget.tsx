02747cc92b5c8d7a52f9d9653634a2f3ad1e3160
Gianna7055 <GRoss10@my.gcu.edu>
2026-04-03 13:58:39 -0700

    Commit start of create budget page (broken)

--- Frontend/MakesCentsFrontend/app/new-edit-screens/create-budget.tsx
+++ Frontend/MakesCentsFrontend/app/new-edit-screens/create-budget.tsx
@@ -1,10 +1,14 @@
+import BottomNavBar from "@/components/bottom-nav-bar";
+import { Button } from "@/components/buttons/button";
+import Input from "@/components/text/text-input";
 import ScreenWrapper from "@/components/ui/screen-wrapper";
 import { globalStyles } from "@/css/globalStyles";
+import { CreateBudgetRequest } from "@/types/create-budget-request";
 import { Month } from "@/types/month";
-import { TransactionType } from "@/types/transaction-type";
-import { useLocalSearchParams } from "expo-router";
-import React from "react";
-import { View, Image, Text } from "react-native";
+import { handleAxiosError } from "@/utils/axiosErrorHandler";
+import { router, useLocalSearchParams } from "expo-router";
+import React, { useState } from "react";
+import { View, Image, Text, ScrollView } from "react-native";
 
 type NewEditBudgetProps = {
   yearString: string;
@@ -19,6 +23,44 @@ export default function NewEditBudget() {
   const year: number = parseInt(yearString);
   const monthInt: number = parseInt(monthString);
   const month: Month = monthInt as Month;
+  const [budgetName, setBudgetName] = useState<string>("");
+
+  const handleCancelClickEH = () => {
+    router.back();
+  };
+
+  const handleDoneClickEH = () => {
+    try {
+        // Create the request
+        const request: CreateBudgetRequest = {
+            
+            budgetName,
+            month,
+            year
+        };
+    } catch (error: any) {
+        handleAxiosError(error);
+    }
+  };
+
+  const renderCancelDoneButtons = () => {
+    return (
+      <View style={globalStyles.bottomButtons}>
+        <Button
+          name="Cancel"
+          onPress={handleCancelClickEH}
+          variant="secondary"
+          containerStyle={{ paddingTop: 0 }}
+        />
+        <Button
+          name="Done"
+          onPress={handleDoneClickEH}
+          variant="primary"
+          containerStyle={{ paddingTop: 0 }}
+        />
+      </View>
+    );
+  };
 
   return (
     <ScreenWrapper>
@@ -28,10 +70,22 @@ export default function NewEditBudget() {
           source={require("@/assets/images/MakesCentsLogo.png")}
           style={globalStyles.noWordsLogo}
         />
-        <Text style={globalStyles.logoTitle}>
-          New Budget
-        </Text>
+        <Text style={globalStyles.logoTitle}>New Budget</Text>
       </View>
+      {/* Content */}
+      <ScrollView style={{ marginVertical: 0 }}>
+        {/* Budget creation form goes here */}
+        <Input
+          name="Budget Name"
+          placeholder="Budget Name"
+          type="text"
+          value={budgetName || ""}
+          onChangeText={setBudgetName}
+          autoCapitalize="words"
+        />
+      </ScrollView>
+      {renderCancelDoneButtons()}
+      <BottomNavBar />
     </ScreenWrapper>
   );
 }
